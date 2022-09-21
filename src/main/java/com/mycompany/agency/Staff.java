c7e87ff6cbcd704ce0798e4c8fe9679cd6b557e8
rinku <sarkr72@mail.sunysuffolk.edu>
2022-09-21 11:01:41 -0400

    added staff members into list

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -4,22 +4,24 @@ public class Staff {
 
     private StaffMember[] staffList;
 
-    //-----------------------------------------------------------------
-    //  Constructor: Sets up the list of staff members.
-    //-----------------------------------------------------------------
     public Staff() {
         staffList = new StaffMember[6];
 
-        // TODO 1: Add 6 emploees to the staffList
-        StaffEmployee se1 = new StaffEmployee("Moaath Alrajab", "202 4th street", "6318482200", "502-555-5555", 50);
-        StaffEmployee se2 = new StaffEmployee("Rinku Sarkar", "50 20th street", "6318485588", "502-222-2222", 59);
-        StaffEmployee se3 = new StaffEmployee("Tifanny chan", "55 1st street", "6312025544", "502-333-3333", 40);
-        StaffEmployee se4 = new StaffEmployee("JOhn doe", "11 20th street", "6318464588", "502-222-2222", 54);
-        StaffEmployee se5 = new StaffEmployee("The Rock", "19 25th street", "6315555588", "502-444-4444", 51);
-        StaffEmployee se6 = new StaffEmployee("Kevin Hart", "80 10th street", "6318489999", "502-888-8888", 56);
+        // done 1: Add 6 emploees to the staffList
+        StaffMember se1 = new StaffMember("Moaath Alrajab", "202 4th street", "6318482200");
+        StaffMember se2 = new StaffMember("Rinku Sarkar", "50 20th street", "6318485588");
+        StaffMember se3 = new StaffMember("Tifanny chan", "55 1st street", "6312025544");
+        StaffMember se4 = new StaffMember("JOhn doe", "11 20th street", "6318464588");
+        StaffMember se5 = new StaffMember("The Rock", "19 25th street", "6315555588");
+        StaffMember se6 = new StaffMember("Kevin Hart", "80 10th street", "6318489999");
+
+        staffList[0] = se1;
+        staffList[1] = se1;
+        staffList[2] = se1;
+        staffList[3] = se1;
+        staffList[4] = se1;
+        staffList[5] = se1;
         
-            staffList[0] = se1;
-      
         ((Executive) staffList[0]).awardBonus(500.00);
 
         ((TempEmploee) staffList[3]).addHours(40);
--- src/main/java/com/mycompany/agency/StaffMember.java
+++ src/main/java/com/mycompany/agency/StaffMember.java
@@ -3,7 +3,7 @@ package com.mycompany.agency;
 
 
 // TODO 1: Make this class work and public
- public abstract class StaffMember
+ class StaffMember
 {
     protected String name;
     protected String address;
